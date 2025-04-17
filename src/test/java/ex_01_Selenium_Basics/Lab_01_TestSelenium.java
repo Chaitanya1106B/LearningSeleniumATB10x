c57318da5612451a0f8a4fbd0beceff87f02d01a
Chaitanya <b.chaitanya1106@gmail.com>
2025-04-17 18:31:56 +0530

    Selenium Start

--- src/test/java/ex_01_Selenium_Basics/Lab_01_TestSelenium.java
+++ src/test/java/ex_01_Selenium_Basics/Lab_01_TestSelenium.java
@@ -1,5 +1,6 @@
 package ex_01_Selenium_Basics;
 
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
@@ -8,7 +9,7 @@ public class Lab_01_TestSelenium {
         @Test
         public void test_VerifyVWOLogin(){
             // Write the code which will execute the perform the ui interactions
-            FirefoxDriver driver = new FirefoxDriver();
+            ChromeDriver driver = new ChromeDriver();
             driver.get("https://app.vwo.com");
             System.out.println(driver.getTitle());
         }
